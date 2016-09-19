ddd58e491e326fe97440c9e042e39824ec3fef41
Miley <miley.gao@outlook.com>
2016-09-20 07:05:17 +0800

    change format

--- src/main/java/WordsMaker.java
+++ src/main/java/WordsMaker.java
@@ -2,7 +2,7 @@
  * Created by genguo on 9/19/16.
  */
 public class WordsMaker {
-    public static void main(String []args) {
-        System.out.println("hello world!");
-    }
+  public static void main(String []args) {
+    System.out.println("hello world!");
+  }
 }
--- src/test/java/WordsMakerTest.java
+++ src/test/java/WordsMakerTest.java
@@ -1,16 +1,16 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import sun.jvm.hotspot.utilities.Assert;
+import org.junit.Assert;
 
 /**
  * Created by genguo on 9/19/16.
  */
 @RunWith(JUnit4.class)
 public class WordsMakerTest {
-    @Test
-    public void testMain() {
-        boolean passed = true;
-        Assert.that(passed, "Failed here");
-    }
+  @Test
+  public void testMain() {
+    boolean passed = false;
+    Assert.assertTrue("Failed here",passed);
+  }
 }
