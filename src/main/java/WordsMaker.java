fbc23a1698810a172e5184ee93287661ba63c7d4
Kingo Wang <wanggenguo@outlook.com>
2016-09-19 17:54:28 +1000

    new project

---
+++ src/main/java/WordsMaker.java
@@ -0,0 +1,8 @@
+/**
+ * Created by genguo on 9/19/16.
+ */
+public class WordsMaker {
+    public static void main(String []args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ src/test/java/WordsMakerTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import sun.jvm.hotspot.utilities.Assert;
+
+/**
+ * Created by genguo on 9/19/16.
+ */
+@RunWith(JUnit4.class)
+public class WordsMakerTest {
+    @Test
+    public void testMain() {
+        boolean passed = true;
+        Assert.that(passed, "Failed here");
+    }
+}
